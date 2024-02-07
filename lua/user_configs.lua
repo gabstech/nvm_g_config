557a28ef2a7418ae1b4ae44fa4a496b6e8a08ff7
Gabino Hernandez <gahergod@hotmail.com>
2024-02-07 11:19:19 -0600

    Configuracion para ocultar los asteriscos y demas simbolos en neorg

--- lua/user_configs.lua
+++ lua/user_configs.lua
@@ -4,7 +4,10 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 
+-- Para ocultar los iconos de neorg
+vim.cmd("set conceallevel=1")
 
+-- Esto para poner los simbolos dobles
 vim.cmd("inoremap { {<CR>}<Esc>ko")
 vim.cmd("inoremap ( ()<Esc>ha")
 vim.cmd("inoremap [ []<Esc>ha")
